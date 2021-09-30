07123bbc21a65755e32299c1012f22559cbbc233
Angel Morcillo leganes <amorcill@1-B-8.42heilbronn.de>
2021-09-30 20:58:57 +0200

    headers, and more things

--- README.md
+++ README.md
@@ -4,16 +4,30 @@
 
 	MiniLibX - Simple Graphical Interface Library
 	man /usr/share/man/man3/mlx.1
+							mlx_loop.1        
+							mlx_new_image.1   
+							mlx_new_window.1  
+							mlx_pixel_put.1
 
 # References
-- https://harm-smits.github.io/42docs/libs/minilibx
-- https://harm-smits.github.io/42docs/libs/minilibx/getting_started.html
+- https://stackoverflow.com/c/42network/questions/531		[mlx]
+
+  
 - https://qst0.github.io/ft_libgfx/man_mlx.html
--	https://opengameart.org/content/dungeon-crawl-32x32-tiles
+- https://github.com/qst0/ft_libgfx#graphics-branch-link-dump
+  
+- https://opengameart.org/content/dungeon-crawl-32x32-tiles
 - https://github.com/agnusmaximus/Mario-World
 - https://convertio.co/png-xpm/
-- void  *mlx_new_window(void *mlx_ptr, int size_x, int size_y, char *title);
-- sze_x is rows size * image size in pixels (25 x 25, use 25) 	Mlx library: man ./mlx.3
-
-
 - https://makefiletutorial.com/
+
+
+
+30-09-2021
+https://harm-smits.github.io/42docs/libs/minilibx [good!!!]
+
+https://github.55860.com/keuhdall/images_example 
+
+
+https://github.com/keuhdall/images_example [info]
+https://github.com/AmberLawr/so_long [code]
--- main.c
+++ main.c
@@ -6,32 +6,92 @@
 /*   By: amorcill <amorcill@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/27 13:37:26 by amorcill          #+#    #+#             */
-/*   Updated: 2021/09/27 13:54:54 by amorcill         ###   ########.fr       */
+/*   Updated: 2021/09/30 20:57:05 by amorcill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <mlx.h>
+#include "so_long.h"
 
-int main (int argc, char **argv)
+// Include for debugging
+//#include "mlx.h"
+//#include <stdlib.h>
+//#include <X11/Xlib.h>
+
+
+
+
+typedef struct	s_img
 {
-	void	*mlx;
-	void	*mlx_win;
+	void		*img_ptr;
+	int			*data; 
+	int			size_l;
+	int			bpp;
+	int			endian;
+}				t_img;
 
-	if ( argc == 2 && argv)
-	{
-		//do somethign
-	}
-		
+typedef struct	s_mlx
+{
+	void		*mlx_instance;
+	void		*win;
+	t_img		img;
+}				t_mlx;
 
-	mlx = mlx_init();
 
-	mlx_win = mlx_new_window(
-		mlx,
-		1920,
-		1080,
-		"Hello World"
-	);
+int main ()
+{
+	//https://harm-smits.github.io/42docs/libs/minilibx/getting_started.html
+	t_mlx	mlx; //Here I first create my struct that will contains all the "MLX stuff"
+	int		count_w;
+	int		count_h;
+
+	count_h = -1;
+	//First you need to call mlx_init and store its return value.
+	mlx.mlx_instance = mlx_init();
+	//Now do the same with mlx_new_window
+	mlx.win = mlx_new_window(mlx.mlx_instance, WIN_WIDTH, WIN_HEIGHT, "A simple shit");
+	//One more time with mlx_new_image
+	mlx.img.img_ptr = mlx_new_image(mlx.mlx_instance, WIN_WIDTH, WIN_HEIGHT);
+	/*
+	 Now the important part :
+	 mlx_get_data_addr will return a char* that is 4 times the (width * height) of your image.
+	 Why so ? Let me explain : This char* will represent your image, pixel by pixel,
+	 and the values of this array are the colors. That's why the array is 4 times bigger :
+	 you need 4 char to code the color of each pixels (one for Red, Green and Blue) and one for the alpha.
+	 But... it's not very convenient, right ? So here is my little trick : you cast
+	 mlx_get_data_addr as an int* and store it in an int*.
+	 This way, the array will have the exact same size as your window, and each index
+	 will represent one complete color of a pixel !
+	*/ 
 	
-
-	return (0);	
+	
+	mlx.img.data = (int *)mlx_get_data_addr(mlx.img.img_ptr, &mlx.img.bpp, &mlx.img.size_l,
+		&mlx.img.endian);
+	/*
+	 Now just a little example : here is a loop that will draw each pixels that
+	 have an odd width in white and the ones that have an even width in black.
+	*/
+	// while (++count_h < WIN_HEIGHT)
+	// {
+	// 	count_w = -1;
+	// 	while (++count_w < WIN_WIDTH)
+	// 	{
+	// 		if (count_w % 2)
+	// 			/*
+	// 			 As you can see here instead of using the mlx_put_pixel function
+	// 			 I just assign a color to each pixel one by one in the image,
+	// 			 and the image will be printed in one time at the end of the loop.
+	// 			 Now one thing to understand here is that you're working on a 1-dimensional
+	// 			 array, while your window is (obviously) 2-dimensional.
+	// 			 So, instead of having data[height][width] here you'll have the following
+	// 			 formula : [current height * max width + current width] (as you can see below)
+	// 			*/
+	// 			mlx.img.data[count_h * WIN_WIDTH + count_w] = 0xFFFFFF;
+	// 		else
+	// 			mlx.img.data[count_h * WIN_WIDTH + count_w] = 0;
+	// 	}
+	// }
+	//Now you just have to print the image using mlx_put_image_to_window !
+	mlx_put_image_to_window(mlx.mlx_instance, mlx.win, mlx.img.img_ptr, 0, 0);
+	mlx_loop(mlx.mlx_instance);
+	return (0);
 }
