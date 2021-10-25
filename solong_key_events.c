0f5aa4044ef1c9654da15e8ead35b651fdc990f8
Angel Morcillo leganes <amorcill@1-B-8.42heilbronn.de>
2021-10-25 20:03:28 +0200

    cleanup

--- solong_key_events.c
+++ solong_key_events.c
@@ -6,7 +6,7 @@
 /*   By: amorcill <amorcill@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/11 14:31:29 by amorcill          #+#    #+#             */
-/*   Updated: 2021/10/19 10:45:33 by amorcill         ###   ########.fr       */
+/*   Updated: 2021/10/25 20:01:14 by amorcill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,17 @@
 #include "so_long.h"
 
 int key_events(int key, t_mlx *mlx)
-{
-	mlx->name = "Game SoLong";
+{	
 	if(key == 0 || key == 123)
 	{
 		player_move_a(mlx);
-		//printf("key press A or <-: %d\n", key);			
 	}
-	if(key == 1 || key == 125){
+	if(key == 1 || key == 125)
+	{
 		player_move_s(mlx);		
 	}
-	if(key == 2 || key == 124){
+	if(key == 2 || key == 124)
+	{
 		player_move_d(mlx);				
 	}	
 	if(key == 13 || key == 126)
@@ -32,8 +32,7 @@ int key_events(int key, t_mlx *mlx)
 		player_move_w(mlx);
 	}	
 	if(key == 53)
-	{
-		// close everzthign
+	{		
 		exit (0);
 	}
 	return (1);
