cfdb61e1f310aeb4b2f717496ff21f186a04e2c5
Angel Morcillo leganes <amorcill@1-B-8.42heilbronn.de>
2021-11-12 18:50:17 +0100

    Cleanup...

--- solong_player.c
+++ solong_player.c
@@ -6,7 +6,7 @@
 /*   By: amorcill <amorcill@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/11 17:44:03 by amorcill          #+#    #+#             */
-/*   Updated: 2021/11/05 13:41:11 by amorcill         ###   ########.fr       */
+/*   Updated: 2021/11/12 18:44:24 by amorcill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -63,29 +63,16 @@ void	player_update(t_mlx *mlx)
 		score_steps(mlx);
 	}
 	if (mlx->bonus == 1 && mlx->map[new_xy[1]][new_xy[0]] == 'G')
-	{
 		ghost_player_crash(mlx, new_xy[0], new_xy[1]);
-	// 	mlx_put_image_to_window(mlx->mlx, mlx->win, mlx->imgs[IMG_PATH_2].img, mlx->player1.horizontal * ZOOM, mlx->player1.vertical * ZOOM);
-	// 	mlx_put_image_to_window(mlx->mlx, mlx->win, mlx->imgs[IMG_PATH_2].img, new_xy[0] * ZOOM, new_xy[1] * ZOOM);
-	// 	mlx->map[new_xy[1]][new_xy[0]] = '0';
-	// 	mlx->player1.lives = 0;
-	// 	score_print(mlx);
-	// 	mlx_string_put(mlx->mlx, mlx->win, (mlx->imap.height / 2) * ZOOM, (mlx->imap.width / 2) * ZOOM, 0x00FFFFFFF, "GAME OVER");
-	}
 	if (mlx->map[new_xy[1]][new_xy[0]] == 'E')
 	{
 		if (collectible_check(mlx) == 0)
 		{
 			map_load_path2(mlx, mlx->player1.horizontal, mlx->player1.vertical);
-			// mlx_put_image_to_window(mlx->mlx, mlx->win, mlx->imgs[IMG_PATH_2].img,
-			// mlx->player1.horizontal * ZOOM, mlx->player1.vertical * ZOOM);
 			mlx->player1.game_on = 0;
-			game_win(mlx);			
-		}			
+			game_win(mlx);
+		}
 	}
 	if (mlx->bonus == 1)
-	{
-		mlx_loop_hook( mlx->mlx, ghost_move, mlx);
-		//ghost_move(mlx);		
-	}
+		mlx_loop_hook(mlx->mlx, ghost_move, mlx);
 }
