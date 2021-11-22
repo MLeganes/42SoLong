3b11ed38fd114357a3c9ce6d2896b1d77a9608b4
Angel Morcillo leganes <amorcill@1-A-1.42heilbronn.de>
2021-11-22 15:41:40 +0100

    Check empty line in map

--- solong_utils.c
+++ solong_utils.c
@@ -6,7 +6,7 @@
 /*   By: amorcill <amorcill@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/12 19:25:05 by amorcill          #+#    #+#             */
-/*   Updated: 2021/11/12 19:28:28 by amorcill         ###   ########.fr       */
+/*   Updated: 2021/11/22 14:54:01 by amorcill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,7 +32,16 @@ void	ft_remove_eol(char *s)
 	return ;
 }
 
-int	ft_count_lines(t_mlx *mlx, char **argv)
+void	ft_check_emptyline(char *line)
+{
+	if ((line != NULL && line[0] == '\0') || (line != NULL && line[0] == '\n'))
+	{
+		error_print_exit("[map error] Error read map. No empty lines in map, "
+			"don't touch Pacman balls!!!");
+	}	
+}
+
+void	ft_count_lines(t_mlx *mlx, char **argv)
 {
 	int		lines;
 	char	*line;
@@ -42,18 +51,19 @@ int	ft_count_lines(t_mlx *mlx, char **argv)
 	lines = 0;
 	fd = open(argv[1], O_RDONLY);
 	if (fd <= 0)
-		return (-2);
+		error_print_exit
+		("[open file error] Error to open file!Check file.");
 	line = get_next_line(fd);
-	ft_remove_eol(line);
+	ft_check_emptyline(line);
 	len = ft_strlen(line);
 	while (line)
 	{
 		free(line);
 		line = get_next_line(fd);
+		ft_check_emptyline(line);
 		lines++;
 	}
 	close(fd);
 	mlx->imap.height = lines;
 	mlx->imap.width = len;
-	return (1);
 }
